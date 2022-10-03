85428fc6a267cf9402ad46d6e8ac21d06de8673b
Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>
2022-10-03 12:08:42 +0300

    Does more cd for bowsword and use only 1 arrow but without ability pickup it in survival

--- src/main/java/content/items/BowSword.java
+++ src/main/java/content/items/BowSword.java
@@ -18,20 +18,21 @@ public class BowSword extends SwordItem {
     public BowSword() {
         super(Tiers.IRON, 3, -3F, new Properties().tab(CreativeModeTab.TAB_COMBAT).durability(225));
         this.time = 0;
-        this.shots_per_second = 3/2F;
+        this.shots_per_second = 1F;
     }
 
     @Override
     public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand hand) {
         ItemStack itemStack = player.getItemInHand(hand);
         ItemStack projectiles = player.getProjectile(new ItemStack(Items.BOW, 1));
-        if (!level.isClientSide && level.getGameTime() - this.time > 20 / this.shots_per_second && (projectiles.getCount() > 2 || player.getAbilities().instabuild)) {
+        if (!level.isClientSide && level.getGameTime() - this.time > 20 / this.shots_per_second && (!projectiles.isEmpty() || player.getAbilities().instabuild)) {
             this.time = level.getGameTime();
             level.playSound((Player) null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.NEUTRAL, 0.5F, 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
             ArrowItem arrowitem = (ArrowItem) (projectiles.getItem() instanceof ArrowItem ? projectiles.getItem() : Items.ARROW);
             used(player, projectiles, itemStack);
             for (float deg = -15F; deg <= 15; deg += 15) {
                 AbstractArrow abstractarrow = arrowitem.createArrow(level, projectiles, player);
+                abstractarrow.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
                 abstractarrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 3.0F, deg);
                 level.addFreshEntity(abstractarrow);
             }
